0477ed3a0945c2d9a2b065007ea0ecb075bc772c
Marc Brunet <marcbrunet@dorna.com>
2020-06-05 13:36:59 +0200

    no mensage

--- edistribucion.py
+++ edistribucion.py
@@ -8,39 +8,45 @@ from prometheus_client import CollectorRegistry, Gauge, push_to_gateway
 
 
 def get_endesa_price(url, registry):
-    r = requests.get(url)
-    html = r.text.splitlines()
-    i = 0
-    if r.status_code == 200:
-        while i < len(html):
-            if "gauge" in html[i]:
-                for x in range(0, 4):
-                    if "€/kWh" in html[i + x]:
-                        preu = html[i + x - 1].split(">")[1]
-                        if preu != '':
-                            tipus = url.split('/')[-1]
-                            if tipus == '':
-                                tipus = 'Normal'
-                            else:
-                                tipus = tipus.split('=')[1]
-                            var = Gauge('preuKW_' + tipus, 'preu €/KW tarifa ' + tipus, registry=registry)
-                            var.set(preu)
-            i += 1
+    try:
+        r = requests.get(url)
+        html = r.text.splitlines()
+        i = 0
+        if r.status_code == 200:
+            while i < len(html):
+                if "gauge" in html[i]:
+                    for x in range(0, 4):
+                        if "€/kWh" in html[i + x]:
+                            preu = html[i + x - 1].split(">")[1]
+                            if preu != '':
+                                tipus = url.split('/')[-1]
+                                if tipus == '':
+                                    tipus = 'Normal'
+                                else:
+                                    tipus = tipus.split('=')[1]
+                                var = Gauge('preuKW_' + tipus, 'preu €/KW tarifa ' + tipus, registry=registry)
+                                var.set(preu)
+                i += 1
+    except Exception as e:
+        print(e)
 
 def get_edistribucion(curl, registry):
-    x = os.popen(curl).read()
-    if None != json.loads(x)['actions'][0]['returnValue']:
-        potenciaActual = Gauge('potenciaActual', 'potencia Actual', registry=registry)
-        potenciaActual.set(json.loads(x)['actions'][0]['returnValue']['data']['potenciaActual'])
+    try:
+        x = os.popen(curl).read()
+        if None != json.loads(x)['actions'][0]['returnValue']:
+            potenciaActual = Gauge('potenciaActual', 'potencia Actual', registry=registry)
+            potenciaActual.set(json.loads(x)['actions'][0]['returnValue']['data']['potenciaActual'])
 
-        totalizador = Gauge('totalizador', 'total enegira consumida per perioda de facturacio ', registry=registry)
-        totalizador.set(json.loads(x)['actions'][0]['returnValue']['data']['totalizador'])
+            totalizador = Gauge('totalizador', 'total enegira consumida per perioda de facturacio ', registry=registry)
+            totalizador.set(json.loads(x)['actions'][0]['returnValue']['data']['totalizador'])
 
-        estadoICP = Gauge('estadoICP', 'estat del ICP del contador ', registry=registry)
-        estadoICP.set(json.loads(x)['actions'][0]['returnValue']['data']['estadoICP'] == "Abierto")
+            estadoICP = Gauge('estadoICP', 'estat del ICP del contador ', registry=registry)
+            estadoICP.set(json.loads(x)['actions'][0]['returnValue']['data']['estadoICP'] == "Abierto")
 
-        potenciaContratada = Gauge('potenciaContratada', 'KW de potencia contractada', registry=registry)
-        potenciaContratada.set(json.loads(x)['actions'][0]['returnValue']['data']['potenciaContratada'])
+            potenciaContratada = Gauge('potenciaContratada', 'KW de potencia contractada', registry=registry)
+            potenciaContratada.set(json.loads(x)['actions'][0]['returnValue']['data']['potenciaContratada'])
+    except Exception as e:
+        print(e)
 
 
 curl = os.environ['EDISTRIBUCION']
