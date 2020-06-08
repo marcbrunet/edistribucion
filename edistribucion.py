351d48d0b5de88468186da693f5cb47f75dc7286
Marc Brunet <marcbrunet@dorna.com>
2020-06-08 11:09:39 +0200

    no mensage

--- edistribucion.py
+++ edistribucion.py
@@ -4,6 +4,7 @@ import os
 import json
 import time
 import requests
+from datetime import datetime
 from prometheus_client import CollectorRegistry, Gauge, push_to_gateway
 
 
@@ -14,18 +15,20 @@ def get_endesa_price(url, registry):
         i = 0
         if r.status_code == 200:
             while i < len(html):
-                if "gauge" in html[i]:
-                    for x in range(0, 4):
-                        if "€/kWh" in html[i + x]:
-                            preu = html[i + x - 1].split(">")[1]
-                            if preu != '':
-                                tipus = url.split('/')[-1]
-                                if tipus == '':
-                                    tipus = 'Normal'
-                                else:
-                                    tipus = tipus.split('=')[1]
-                                var = Gauge('preuKW_' + tipus, 'preu €/KW tarifa ' + tipus, registry=registry)
-                                var.set(preu)
+                hora = datetime.now().hour
+                formathora = "{}h - ".format(hora)
+                if formathora in html[i]:
+                    for elemnt in html[i].split(" "):
+                        if 'itemprop="price">' in elemnt:
+                            tipus = url.split('/')[-1]
+                            if tipus == '':
+                                tipus = 'Normal'
+                            else:
+                                tipus = tipus.split('=')[1]
+                            print(tipus)
+                            var = Gauge('preu2KW_' + tipus, 'preu €/KW tarifa ' + tipus, registry=registry)
+                            var.set(elemnt.split('>')[1])
+                            i = len(html)
                 i += 1
     except Exception as e:
         print(e)
@@ -52,16 +55,16 @@ def get_edistribucion(curl, registry):
 curl = os.environ['EDISTRIBUCION']
 push = os.environ['PUSHGATEWAY']
 while True:
-        registry = CollectorRegistry()
-        # declare metrics
-        g = Gauge('job_last_success_unixtime', 'Last time a batch job successfully finished', registry=registry)
-        g.set_to_current_time()
+    registry = CollectorRegistry()
+    # declare metrics
+    g = Gauge('job_last_success_unixtime', 'Last time a batch job successfully finished', registry=registry)
+    g.set_to_current_time()
 
-        get_endesa_price("https://tarifaluzhora.es/", registry)
-        get_endesa_price("https://tarifaluzhora.es/?tarifa=discriminacion", registry)
-        get_endesa_price("https://tarifaluzhora.es/?tarifa=coche_electrico", registry)
+    get_endesa_price("https://tarifaluzhora.es/", registry)
+    get_endesa_price("https://tarifaluzhora.es/?tarifa=discriminacion", registry)
+    get_endesa_price("https://tarifaluzhora.es/?tarifa=coche_electrico", registry)
 
-        get_edistribucion(curl, registry)
+    get_edistribucion(curl, registry)
 
-        push_to_gateway(push, job='E-energia', registry=registry)
-        time.sleep(30)
+    push_to_gateway(push, job='E-energia', registry=registry)
+    time.sleep(30)
